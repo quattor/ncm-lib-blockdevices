1b3963b80cc99f0694ced0311da75159c5b89f9d
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2009-11-25 15:13:47 +0000

    Add support for Tmpfs
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lib/blockdevices@1367 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- src/main/perl/BlockdevFactory.pm
+++ src/main/perl/BlockdevFactory.pm
@@ -19,6 +19,7 @@ use NCM::LV;
 use NCM::Disk;
 use NCM::Partition;
 use NCM::File;
+use NCM::Tmpfs;
 use constant BASEPATH	=> "/system/blockdevices/";
 use constant PARTED	=> qw (/sbin/parted -s --);
 use constant PARTEDP	=> 'p';
@@ -37,49 +38,61 @@ Returns the object describing the block device passed as an argument.
 
 sub build
 {
-	my ($config, $dev) = @_;
+    my ($config, $dev) = @_;
 
-	if ($dev =~ m!^volume_groups/.*!) {
-		return NCM::LVM->new (BASEPATH . $dev, $config);
-	} elsif ($dev =~ m!^md/.*!) {
-		return NCM::MD->new (BASEPATH . $dev, $config);
-	} elsif ($dev =~ m!^partitions/.*!) {
-		return NCM::Partition->new (BASEPATH . $dev, $config);
-	} elsif ($dev =~ m!^physical_devs/.*!) {
-		return NCM::Disk->new (BASEPATH . $dev, $config);
-	} elsif ($dev =~ m!^files/.*!) {
-		return NCM::File->new (BASEPATH . $dev, $config);
-	} elsif ($dev =~ m!^logical_volumes/.*!) {
-		return NCM::LV->new (BASEPATH . $dev, $config);
-	}
-	# WTF?
-	return undef;
+    if ($dev =~ m!^volume_groups/.*!) {
+	return NCM::LVM->new (BASEPATH . $dev, $config);
+    }
+    elsif ($dev =~ m!^md/.*!) {
+	return NCM::MD->new (BASEPATH . $dev, $config);
+    }
+    elsif ($dev =~ m!^partitions/.*!) {
+	return NCM::Partition->new (BASEPATH . $dev, $config);
+    }
+    elsif ($dev =~ m!^physical_devs/.*!) {
+	return NCM::Disk->new (BASEPATH . $dev, $config);
+    }
+    elsif ($dev =~ m!^files/.*!) {
+	return NCM::File->new (BASEPATH . $dev, $config);
+    }
+    elsif ($dev =~ m!^logical_volumes/.*!) {
+	return NCM::LV->new (BASEPATH . $dev, $config);
+    }
+    elsif ($dev eq "tmpfs") {
+	return NCM::Tmpfs->new(BASEPATH . $dev, $config);
+    }
+    # WTF?
+    return undef;
 }
 
 sub build_from_dev
 {
-	my ($dev, $config) = @_;
+    my ($dev, $config) = @_;
 
-	$this_app->debug (5, "Creating block device structure for $dev device");
-	if ($dev =~ m{^/dev/md\d+$}) {
-		return NCM::MD->new_from_system ($dev, $config);
-	} elsif (($dev =~ m{^/dev/mapper/}) ||
-		 (-l $dev && (my $rd = readlink ($dev)) =~ m{^/dev/mapper})) {
-		# Check this one out!!
-		return NCM::LV->new_from_system (defined $rd? $rd:$dev, $config);
-	} elsif ($dev =~ m{^/dev/}) {
-		# This is the most generic way I can think of deciding
-		# whether a path refers to a full disk or to a
-		# partition.
-		output (PARTED, $dev, PARTEDP);
-		if ($?) {
-			return NCM::Disk->new_from_system ($dev, $config);
-		} else {
-			return NCM::Partition->new_from_system ($dev, $config);
-		}
-	} else  {
-		return NCM::File->new_from_system ($dev, $config);
+    $this_app->debug (5, "Creating block device structure for $dev device");
+    if ($dev =~ m{^/dev/md\d+$}) {
+	return NCM::MD->new_from_system ($dev, $config);
+    }
+    elsif (($dev =~ m{^/dev/mapper/}) ||
+		(-l $dev && (my $rd = readlink ($dev)) =~ m{^/dev/mapper})) {
+	# Check this one out!!
+	return NCM::LV->new_from_system (defined $rd? $rd:$dev, $config);
+    }
+    elsif ($dev =~ m{^/dev/}) {
+	# This is the most generic way I can think of deciding
+	# whether a path refers to a full disk or to a
+	# partition.
+	output (PARTED, $dev, PARTEDP);
+	if ($?) {
+	    return NCM::Disk->new_from_system ($dev, $config);
 	}
+	else {
+	    return NCM::Partition->new_from_system ($dev, $config);
+	}
+    }
+    else {
+	return NCM::File->new_from_system ($dev, $config);
+    }
 }
 
 1;
